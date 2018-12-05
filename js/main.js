17886a1a3459abb853ad873e581166f41044395a
Gene0 <445119186@qq.com>
2018-12-05 12:43:30 +0800

    添加了制作过程

---
+++ js/main.js
@@ -0,0 +1,168 @@
+!function(){
+    function writeCode(prefix, code, fn){
+        let container = document.querySelector('#code')
+        let styleTag = document.querySelector('#styleTag')
+        let n = 0 
+        let timer = setInterval(function(){
+            n+=1
+            container.innerHTML = code.slice(0, n)
+            styleTag.innerHTML = code.slice(0, n)
+            container.scrollTop = container.scrollHeight
+            if(n >= code.length){
+                clearInterval(timer)
+                fn && fn.call()
+            }
+        },10)
+    }
+let code = `/*
+*让我们先准备皮卡丘的皮
+*/
+.preview{
+    height:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background: #FFE600;
+    border:1px solid green;
+}
+.code-wrapper{
+    flex:1;
+    height:50%;
+}
+.preview-wrapper{
+    flex:1;
+    height:50%;
+}
+.wrapper{
+    width:420px;
+    height:238px;
+    position:relative;
+}
+/*
+*这是他的鼻子
+*/
+.nose{
+    width:0px;
+    height:0px;
+    border:13px solid black;
+    border-radius:13px;
+    border-color:black transparent transparent;
+    position:absolute;
+    left:50%;
+    top:35px;
+    margin-left:-13px;
+}
+/*
+*这是眼睛
+*/
+.eye{
+    width:64px;
+    height:64px;
+    background:#2E2E2E;
+    position:absolute;
+    border:2px solid #000;
+    border-radius:50%;
+}
+.eye::before{
+    content:'';
+    display:block;
+    width:26px;
+    height:26px;
+    background:white;
+    border-radius:50%;
+    border:1px silid #030303;
+    position:absolute;
+    left:10px;
+    top:2px;
+}
+.eye.left{
+    right:50%;
+    margin-right:88px;
+}
+.eye.right{
+    left:50%;
+    margin-left:88px;
+}
+/*
+*加点红的
+*/
+.face{
+    width:88px;
+    height:88px;
+    background:#FF0000;
+    border:2px solid #000;
+    border-radius:50%;
+    position:absolute;
+    top:112px;
+}
+.face.left{
+    right:50%;
+    margin-right:122px;
+}
+.face.right{
+    left:50%;
+    margin-left:122px;
+}
+/*
+*这是上嘴唇
+*/
+.UpperLip{
+    width:100px;
+    height:30px;
+    border:3px solid #000;
+    position:absolute;
+    top:65px;
+    background: #FFE600;
+}
+.UpperLip.left{
+    border-bottom-left-radius:60px 30px;
+    border-top:none;
+    border-right:none;
+    transform:rotate(-25deg);
+    right:50%;
+}
+.UpperLip.right{
+    border-bottom-right-radius:60px 30px;
+    border-top:none;
+    border-left:none;
+    left:50%;
+    transform:rotate(25deg);
+}
+.LowerLip-wrapper{
+    width:420px;
+    height:165px;
+    position:absolute;
+    bottom:0;
+    left:50%;
+    margin-left:-110px;
+    overflow: hidden;
+}
+/*
+*这是下嘴唇
+*/
+.LowerLip{
+    width:220px;
+    height:1400px;
+    border:2px solid black;
+    border-radius:60%;
+    background:#9B000A;
+    position:absolute;
+    bottom:0;
+    overflow: hidden;
+}
+.LowerLip::after{
+    content: '';
+    position: absolute;
+    bottom:0;
+    width:140px;
+    height:140px;
+    background:#FF485F;
+    left:50%;
+    margin-left:-70px;
+    border-radius:50%;
+}
+/*
+*做好啦,是不是很可爱呀!!!
+*/`
+    writeCode('',code)
+}.call()
